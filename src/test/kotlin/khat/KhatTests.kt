becd1c9e27cbc190bdce3f0fd43c48ff0fd2e4d9
Jérôme Velociter <jerome@velociter.fr>
2015-08-01 12:47:05 +0200

    SQL testing scaffholding

---
+++ src/test/kotlin/khat/KhatTests.kt
@@ -0,0 +1,117 @@
+package khat
+
+import org.junit.Assert
+import org.junit.ClassRule
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.ExternalResource
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+import org.junit.runners.model.Statement
+import org.postgresql.ds.PGPoolingDataSource
+import java.lang.annotation.ElementType
+import java.lang.annotation.Retention
+import java.lang.annotation.RetentionPolicy
+import java.lang.annotation.Target
+import java.util.function.Supplier
+import javax.sql.DataSource
+import kotlin.platform.platformStatic
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+annotation class SqlBefore(val sql: String = "SELECT 1")
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+annotation class SqlAfter(val sql: String = "SELECT 1")
+
+class SqlTest(dsp: Supplier<DataSource>): TestWatcher() {
+
+    val dataSource: DataSource
+
+    init {
+        dataSource = dsp.get()
+    }
+
+    override fun starting(description: Description?) {
+        val sqlBefore = description?.getAnnotation(javaClass<SqlBefore>());
+        if (sqlBefore != null) {
+            val statement = dataSource.getConnection().createStatement()
+            statement.execute(sqlBefore!!.sql)
+        }
+    }
+
+    override fun finished(description: Description?) {
+        val sqlAfter = description?.getAnnotation(javaClass<SqlAfter>());
+        if (sqlAfter != null) {
+            val statement = dataSource.getConnection().createStatement()
+            statement.execute(sqlAfter!!.sql)
+        }
+    }
+}
+
+class MyClassRule(dsp: Supplier<DataSource>) : ExternalResource() {
+
+    var sqlBefore: SqlBefore? = null
+    var sqlAfter: SqlAfter? = null
+
+    val dataSource: DataSource
+
+    init {
+        dataSource = dsp.get()
+    }
+
+    override fun apply(statement: Statement?, description: Description?): Statement? {
+        sqlBefore = description?.getAnnotation(javaClass<SqlBefore>());
+        sqlAfter = description?.getAnnotation(javaClass<SqlAfter>());
+        return super.apply(statement, description)
+    }
+
+    override fun before() {
+        if (sqlBefore != null) {
+            val statement = dataSource.getConnection().createStatement()
+            statement.execute(sqlBefore!!.sql)
+        }
+    }
+
+    override fun after() {
+        if (sqlAfter != null) {
+            val statement = dataSource.getConnection().createStatement()
+            statement.execute(sqlAfter!!.sql)
+        }
+    }
+}
+
+open class KhatTests {
+
+    Rule
+    public fun getSql(): SqlTest = _sql
+    val _sql = SqlTest(Companion.dataSource)
+
+    companion object {
+        val dataSource = object: Supplier<DataSource> {
+            private val source: PGPoolingDataSource
+
+            init {
+                source = PGPoolingDataSource();
+                this.initialize();
+            }
+
+            fun initialize() {
+                source setDataSourceName "A Data Source"
+                source setServerName "localhost"
+                source setDatabaseName "khat_tests"
+                source setUser "postgres"
+                source setPassword ""
+                source setMaxConnections 10
+            }
+
+            override fun get(): DataSource {
+                return this.source
+            }
+        }
+
+        @ClassRule
+        val classSql: MyClassRule = MyClassRule(dataSource)
+    }
+}
---
+++ src/test/kotlin/khat/ResultSetMappersTests.kt
@@ -0,0 +1,22 @@
+package khat
+
+import org.junit.Assert
+import org.junit.Test
+
+@SqlBefore("""
+    DROP TABLE IF EXISTS documents;
+    CREATE TABLE documents (id uuid, doc jsonb)
+""")
+@SqlAfter("DROP TABLE documents")
+public class ResultSetMappersTests : KhatTests() {
+
+    @SqlBefore("""
+        INSERT INTO documents VALUES ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', '{"a": "b", "c": 2}');
+        INSERT INTO documents VALUES ('3c75d791-a654-421c-b442-768a4748dff4', '{"toto":  "tata"}');
+    """)
+    @SqlAfter("TRUNCATE TABLE documents")
+    Test fun testMapper() {
+        Assert.assertEquals(1, 2)
+    }
+
+}
